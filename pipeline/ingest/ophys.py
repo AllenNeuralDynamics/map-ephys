9bb378916c68ebddeafac1184da3f4f6f7af27cf
han-lab <houhan@gmail.com>
2022-07-13 11:47:23 -0700

    WIP: add align_phys_to_behav_trials (reusable for ephys as well); add TTL tolerance; TODO: do insertion

--- pipeline/ingest/ophys.py
+++ pipeline/ingest/ophys.py
@@ -1,3 +1,4 @@
+from ctypes import alignment
 import os
 import logging
 import pathlib
@@ -20,6 +21,16 @@ schema = dj.schema(get_schema_name('ingest_ophys'))
 
 log = logging.getLogger(__name__)
 
+
+TTL_trial_event_length = {'bitcodestart': 20,
+                          'go': 1,
+                          'reward': 30,
+                          'trialend': 40,
+                          }
+TTL_bar_code_length = 20
+TTL_action_event_length = {'choiceL': 2,
+                           'choiceR': 3}
+TTL_length_tolerance = 0.1  # Allow a 10% error in TTL width
 
 @schema
 class OphysIngest(dj.Imported):
@@ -145,7 +156,7 @@ class OphysIngest(dj.Imported):
             # TODO: --- Insert ophys.TrialEvent and ophys.ActionEvent ---
             
             ### Kenta 2022/07/05- added ###
-            log.info(f'  Ingest TTL (from BPOD recorded with NIDAQ through Bonsai) data...')
+            log.info(f'  Parse TTL (from BPOD recorded with NIDAQ through Bonsai) data...')
             
             TTLsignal_file = list(ophys_dir.glob('TTL_20*'))[0]
             TTLsignal = np.fromfile(TTLsignal_file)
@@ -184,36 +195,143 @@ class OphysIngest(dj.Imported):
                             break
             log.info(f'     Done!')
             
-            # 1. Decode events and the barcode  
-            log.info(f'  Decoding Trial Barcode for sync with behavior')
-            BarcodeP = TTL_p[TTL_l == 20]         #Barcode starts with a 20ms pulse
-            BarcodeBin = np.zeros((len(BarcodeP),20)) # matrix of 0 or 1, size = (trial#,20)
+            # 1. Decode barcode and events
+            log.info(f'  Decoding trial barcode and events')
+            
+            # Barcode
+            BarcodeP = [p for p, l in zip(TTL_p, TTL_l) if l >= TTL_bar_code_length * (1 - TTL_length_tolerance) 
+                                                       and l <= TTL_bar_code_length * (1 + TTL_length_tolerance)]         #Barcode starts with a 20ms pulse
+            BarcodeBin = np.zeros((len(BarcodeP), TTL_bar_code_length)) # matrix of 0 or 1, size = (trial#,20)
 
             for ii in range(len(BarcodeP)):
                 for jj in range(20):
-                    BarcodeBin[ii,jj] = TTLsignal1[int(BarcodeP[ii])+30+20*jj+5] # checking the middle of 10ms windows
+                    BarcodeBin[ii,jj] = TTLsignal1[int(BarcodeP[ii] + TTL_bar_code_length * (1.5 + jj) + 5)] # checking the middle of 10ms windows
 
-            BarChar=[]  # This will be the list (size = trialN) of 20 char (eg. "01011001001010010100")  
+            ophys_barcode = []  # This will be the list (size = trialN) of 20 char (eg. "01011001001010010100")  
 
-            for ii in range(len(BarcodeP)):
-                temp=BarcodeBin[ii].astype(int)
-                temp2=''
+            for bar_code_bin in BarcodeBin:                   
+                ophys_barcode.append(''.join([str(int(b)) for b in bar_code_bin]))
                 
-                for jj in range(20):
-                    temp2 = temp2 + str(temp[jj])
-                    
-                BarChar.append(temp2)
+            # Events
+            trial_events = {}
+            for trial_event_type, length in TTL_trial_event_length.items():
+                trial_events[trial_event_type] = np.array(TTL_p)[(np.array(TTL_l) >= length * (1 - TTL_length_tolerance))
+                                                               & (np.array(TTL_l) <= length * (1 + TTL_length_tolerance))] 
                 
-                del temp, temp2
-                
+            action_events = {}
+            for action_event_type, length in TTL_action_event_length.items():
+                action_events[action_event_type] = np.array(TTL_p)[(np.array(TTL_l) >= length * (1 - TTL_length_tolerance))
+                                                                 & (np.array(TTL_l) <= length * (1 + TTL_length_tolerance))] 
+                                                                
             log.info(f'     Done!')    
             ### Kenta 2022/07/05- added ###
             # BarChar to be directly compared to behavior
             
             # 2. Align barcode to behavior
-            # 3. Do insertion
+            log.info(f'  Align ophys trials to behavior using barcode...') 
             
+            # Do trial alignment
+            behav_trialN, behav_barcode = (experiment.TrialNote & key & 'trial_note_type = "bitcode"').fetch('trial', 'trial_note', order_by='trial')
+            trial_aligned = align_phys_to_behav_trials(ophys_barcode, list(behav_barcode), list(behav_trialN))
             
-            pass
+            if trial_aligned['perfectly_aligned']:
+                log.info(f'  perfectly aligned!')
+            else:
+                log.info(f'    aligned blocks found:\n'
+                         f'      ophys: {[f"{s}-{e}" for s,e in trial_aligned["phys_aligned_blocks"]]}\n'
+                         f'      behav: {[f"{s}-{e}" for s,e in trial_aligned["behav_aligned_blocks"]]}\n'
+                         f'    ophys not in behav: {trial_aligned["phys_not_in_behav"]}\n'
+                         f'    behav not in ophys: {trial_aligned["behav_not_in_phys"]}')
+                        
+            # 3. Insert events
+            all_bitcodestart = trial_events['bitcodestart']
+            all_bitcodestart = np.append(all_bitcodestart, np.inf)
+            all_trialevent_to_insert = []
+            all_actionevent_to_insert = []
+                                  
+            for ophys_trial, behav_trial in trial_aligned['phys_to_behav_mapping']:  # Only iterate over the aligned trials
+                # Use bitcodestart to separate trials
+                start_this_trial = [ophys_trial - 1]
+                start_next_trial = trial_events['bitcodestart'][ophys_trial]
+                
+                # Add trial events of this trial
+                for trial_event_type, times in trial_events.items():
+                    pass  # TODO: add items to all_trialevent_to_insert
+                
+                # Add action events of this trial
+                for action_event_type, times in action_events.items():
+                    pass  # TODO: add items to all_actionevent_to_insert
+            
+            pass  # TODO: insert table ophys.TrialEvent and ophys.ActionEvent
         
             
+def align_phys_to_behav_trials(phys_barcode, behav_barcode, behav_trialN=None):
+    '''
+    Align physiology trials (ephys/ophys) to behavioral trials using the barcode
+    
+    Input: phys_barcode (list), behav_barcode (list), behav_trialN (list)
+    Output: a dictionary with fields
+        'phys_to_behav_mapping': a list of trial mapping [phys_trialN, behav_trialN]. Use this to trialize events in phys recording
+        'phys_not_in_behav': phys trials that are not found in behavioral trials
+        'behav_not_in_phys': behavioral trials that are not found in phys trials
+        'phys_aligned_blocks': blocks of consecutive phys trials that are aligned with behav
+        'behav_aligned_blocks': blocks of consecutive behav trials that are aligned with phys (each block has the same length as phys_aligned_blocks)
+        'perfectly_aligned': whether phys and behav trials are perfectly aligned
+        
+    '''
+    
+    if behav_trialN is None:
+        behav_trialN = np.r_[1:len(behav_barcode) + 1]
+    else:
+        behav_trialN = np.array(behav_trialN)
+        
+    behav_barcode = np.array(behav_barcode)
+    
+    phys_to_behav_mapping = []  # A list of [phys_trial, behav_trial]
+    phys_not_in_behav = []  # Happens when the bpod protocol is terminated during a trial (incomplete bpod trial will not be ingested to behavior)
+    behav_not_in_phys = []  # Happens when phys recording starts later or stops earlier than the bpod protocol
+    behav_aligned_blocks = []  # A list of well-aligned blocks
+    phys_aligned_blocks = []  # A list of well-aligned blocks
+    behav_aligned_last = -999
+    phys_aligned_last = -999
+    in_a_continous_aligned_block = False # A flag indicating whether the previous phys trial is in a continuous aligned block
+                
+    for phys_trialN_this, phys_barcode_this in zip(range(1, len(phys_barcode + ['fake']) + 1), phys_barcode + ['fake']):   # Add a fake value to deal with the boundary effect
+        behav_trialN_this = behav_trialN[behav_barcode == phys_barcode_this]
+        assert len(behav_trialN_this) <= 1  # Otherwise bitcode must be problematic
+        
+        if len(behav_trialN_this) == 0 or behav_trialN_this - behav_aligned_last > 1:  # The current continuously aligned block is broken
+            # Add a continuously aligned block
+            if behav_aligned_last != -999 and phys_aligned_last != -999 and in_a_continous_aligned_block:
+                behav_aligned_blocks.append([behav_aligned_block_start_this, behav_aligned_last])
+                phys_aligned_blocks.append([phys_aligned_block_start_this, phys_aligned_last])
+                
+            in_a_continous_aligned_block = False
+            
+        if len(behav_trialN_this) == 0:
+            phys_not_in_behav.append(phys_trialN_this)
+        else:
+            phys_to_behav_mapping.append([phys_trialN_this, behav_trialN_this[0]])  # The main output
+            
+            # Cache the last behav-phys matched pair
+            behav_aligned_last = behav_trialN_this[0]
+            phys_aligned_last = phys_trialN_this
+            
+            # Cache the start of each continuously aligned block
+            if not in_a_continous_aligned_block:  # A new continuous block just starts
+                behav_aligned_block_start_this = behav_trialN_this[0]
+                phys_aligned_block_start_this = phys_trialN_this
+            
+            # Switch on the flag
+            in_a_continous_aligned_block = True
+            
+    phys_not_in_behav.pop(-1)  # Remove the last fake value
+    behav_not_in_phys = list(np.setdiff1d(behav_trialN, [b for _, b in phys_to_behav_mapping]))
+    
+    return {'phys_to_behav_mapping': phys_to_behav_mapping,
+            'phys_not_in_behav': phys_not_in_behav,
+            'behav_not_in_phys': behav_not_in_phys,
+            'phys_aligned_blocks': phys_aligned_blocks,
+            'behav_aligned_blocks': behav_aligned_blocks,
+            'perfectly_aligned': len(phys_not_in_behav + behav_not_in_phys) == 0
+            }
