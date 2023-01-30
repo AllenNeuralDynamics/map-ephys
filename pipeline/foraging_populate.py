3789316dbe69f54f8cf6b2d5a7fab48a8517834b
houhan@gmail.com <han.hou@alleninstitute.org>
2023-01-30 02:28:12 +0000

    add progress with thread timer

--- pipeline/foraging_populate.py
+++ pipeline/foraging_populate.py
@@ -1,7 +1,12 @@
 import datajoint as dj
 from datetime import datetime
+
+import sys
+sys.path.append('/root/capsule/code')
+
 from pipeline import lab, get_schema_name, foraging_analysis, report, psth_foraging, foraging_model, ephys, experiment
 import multiprocessing as mp
+from threading import Timer, Thread
 
 # Ray does not support Windows, use multiprocessing instead
 use_ray = False
@@ -42,7 +47,7 @@ def populatemytables_core(arguments, runround):
     for table in my_tables[runround]:
         table.populate(**arguments)
         
-def show_progress(rounds):
+def show_progress(rounds=range(len(my_tables))):
     print('\n--- Current progress ---', flush=True)
     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), flush=True)
     for runround in rounds:
@@ -61,7 +66,7 @@ def show_progress(rounds):
     print('------------------------\n', flush=True)
         
 def populatemytables(pool = None, cores = 9, all_rounds = range(len(my_tables))):
-    show_progress(all_rounds)
+    # show_progress(all_rounds)
     
     if pool is not None:
         # schema = dj.schema(get_schema_name('foraging_analysis'),locals())
@@ -78,7 +83,7 @@ def populatemytables(pool = None, cores = 9, all_rounds = range(len(my_tables)))
 
             print('  round '+ str(runround)+'  done...', flush=True)
     
-        show_progress(all_rounds)
+        # show_progress(all_rounds)
         
     # Just in case there're anything missing?          
     print('--- Run with single core...', flush=True)
@@ -88,7 +93,42 @@ def populatemytables(pool = None, cores = 9, all_rounds = range(len(my_tables)))
         populatemytables_core(arguments, runround)
         
     # Show progress
-    show_progress(all_rounds)
+    # show_progress(all_rounds)
+    
+    
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+            
+def run_with_progress(run_count=1, print_interval=10):
+    
+    cores = int(mp.cpu_count()) - 1  # Auto core number selection
+    print(f'# workers = {cores}')
+    pool = mp.Pool(processes=cores)
+    
+    # t1 = threading.Thread(target=populatemytables, 
+    #                       kwargs=dict(pool=pool, cores=cores, all_rounds=range(len(my_tables)))
+    # )
+    
+    t2 = RepeatTimer(10.0, show_progress)
+    
+    # t1.start()
+    t2.start()
+    
+    while run_count:
+        try:
+            run_count -= 1
+            populatemytables(pool=pool, cores=cores, all_rounds=range(len(my_tables)))
+        except:
+            pass
+    
+    # t1.join()
+    t2.join()
+    
+    if pool != '':
+        pool.close()
+        pool.join()
             
             
 if __name__ == '__main__' and use_ray == False:  # This is a workaround for mp.apply_async to run in Windows
@@ -97,11 +137,6 @@ if __name__ == '__main__' and use_ray == False:  # This is a workaround for mp.a
     # shell.logsetup('INFO')
     # shell.ingest_foraging_behavior()
     
-    cores = int(mp.cpu_count()) - 1  # Auto core number selection
-    pool = mp.Pool(processes=cores)
+    run_with_progress()
     
-    populatemytables(pool=pool, cores=cores, all_rounds=range(len(my_tables)))
-    
-    if pool != '':
-        pool.close()
-        pool.join()
+ 
