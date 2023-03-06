38a3642249950246fe3b3d9cb300a5d5d6f5bed1
houhan@gmail.com <han.hou@alleninstitute.org>
2023-03-06 20:16:27 +0000

    update to_s3 for logistic regressions

--- pipeline/export/to_s3.py
+++ pipeline/export/to_s3.py
@@ -184,9 +184,12 @@ def export_df_regressions(s3_rel_path='st_cache/'):
     df_linear_regression_rt = pd.DataFrame(foraging_analysis_and_export.SessionLinearRegressionRT.Param.fetch())
     export_df_and_upload(df_linear_regression_rt, s3_rel_path, file_name='df_linear_regression_rt.pkl')
 
-    df_logistic_regression = pd.DataFrame((foraging_analysis_and_export.SessionLogisticRegression & 'trials_back <= 10').fetch())
-    export_df_and_upload(df_logistic_regression, s3_rel_path, file_name='df_logistic_regression.pkl')
+    df_logistic_regression_hattori = pd.DataFrame((foraging_analysis_and_export.SessionLogisticRegressionHattori & 'trials_back <= 10').fetch())
+    export_df_and_upload(df_logistic_regression_hattori, s3_rel_path, file_name='df_logistic_regression_hattori.pkl')
     
+    df_logistic_regression_su = pd.DataFrame((foraging_analysis_and_export.SessionLogisticRegressionSu & 'trials_back <= 10').fetch())
+    export_df_and_upload(df_logistic_regression_su, s3_rel_path, file_name='df_logistic_regression_su.pkl')
+   
     return
 
     
